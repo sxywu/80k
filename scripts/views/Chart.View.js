62b49cc6d89894a92b332b44bbb96f6db798e0b0
Shirley Wu <swu@swu-mbp15.local>
2013-10-10 23:33:57 -0700

    dots and lines on the stack bar chart to denote cost of living.  also put in pension contribution figures

--- scripts/views/Chart.View.js
+++ scripts/views/Chart.View.js
@@ -54,27 +54,13 @@ define([
                 gotCosts = true;
             }
         },
-        // renderPositions: function() {
-        //     var that = this,
-        //         max = _.chain(this.positions.models)
-        //             .map(function(model) { 
-        //                 return model.total();
-        //             }).max().value();
-
-        //     this.positions.each(function(model, i) {
-        //         var data = {bars: model.processData(), transform: i},
-        //             component = new StackedBarVisualization();
-
-        //         component.data(data).max(max);
-        //         component(that.el);
-
-        //     });
-        // },
         processData: function() {
             var position = this.positions.getPosition(),
                 cost = this.costs.getCost(),
                 proposal = this.proposals.getProposal(),
                 raises = _.chain(proposal.getLineData().raise)
+                    .flatten().groupBy("party").value(),
+                pensions = _.chain(proposal.getLineData().pension)
                     .flatten().groupBy("party").value(),
                 lastBART = _.clone(position.attributes),
                 lastUnion = _.clone(position.attributes),
@@ -86,10 +72,12 @@ define([
                 }],
                 that = this;
 
-            _.each(raises.BART, function(val, i) {
+            _.each([0, 1, 2, 3], function(i) {
                 var obj = {};
-                lastBART.base += lastBART.base * val.rate;
+                lastBART.base += (raises.BART[i] ? (lastBART.base * raises.BART[i].rate) : 0);
+                lastBART.pension = (pensions.BART[i] ? (lastBART.base * pensions.BART[i].rate) : lastBART.pension);
                 lastUnion.base += (raises.Union[i] ? (lastUnion.base * raises.Union[i].rate) : 0);
+                lastUnion.pension = (pensions.Union[i] ? (lastUnion.base * pensions.Union[i].rate) : lastUnion.pension);
                 obj.bars = [that.processPositionData(lastBART, "BART"), that.processPositionData(lastUnion, "Union")];
                 obj.cost = costTotal * Math.pow(inflation, i + 1);
 
@@ -103,18 +91,19 @@ define([
         processPositionData: function(attributes, party) {
             var order = ["base", "other", "pension", "medical"],
                 starting = 0,
-                opacity = 1 / (order.length * 1.5),
+                opacity = 1 / order.length,
                 data = [];
 
             _.each(order, function(key, i) {
                 var obj = {};
                 obj.starting = starting;
                 obj.ending = starting + attributes[key];
+                obj.height = attributes[key];
                 obj.opacity = 1 - opacity * i;
                 obj.title = key;
                 obj.party = party;
 
-                starting = obj.ending;
+                starting = obj.ending + 1;
 
                 data.push(obj);
             });
--- scripts/visualizations/StackedBar.Visualization.js
+++ scripts/visualizations/StackedBar.Visualization.js
@@ -1,19 +1,23 @@
 define([
     "jquery",
     "underscore",
-    "d3"
+    "d3",
+    "d3.tip",
+    "text!app/templates/RectangleHover.Template.html"
 ], function(
     $,
     _,
-    d3
+    d3,
+    tip,
+    RectangleHoverTemplate
 ) {
     return function() {
         var data,
             max = 150000,
             width = 500,
-            barWidth = 10,
-            height = 300,
-            barPadding = 1,
+            barWidth = 13,
+            height = 350,
+            barPadding = 0,
             padding = 50;
 
         
@@ -23,6 +27,12 @@ define([
             var scale = d3.scale.linear()
                 .domain([0, max])
                 .range([0, height]);
+
+            var rectTip = d3.tip().attr('class', 'd3-tip')
+                .direction("e").offset([0, barWidth / 2])
+                .html(function(d, i) {
+                    return _.template(RectangleHoverTemplate, d); 
+                })
 
 
             // data = data[0].bars[0].bar;
@@ -37,30 +47,42 @@ define([
                 .enter().append("g").classed("bar", true)
                 .attr("transform", function(d, i) {return "translate(" + (i * (barWidth + 2 * barPadding)) + ", 0)"});
 
-            var rects = bars.selectAll("rect").data(function(d) {return d}).enter().append("rect")
-                .attr("x", padding)
+            var rects = bars.selectAll("rect").data(function(d) {return d;}).enter().append("rect")
+                .attr("x", barPadding)
                 .attr("y", function(d) {return height - scale(d.ending)})
                 .attr("width", barWidth)
-                .attr("height", function(d) {return scale(d.ending) - scale(d.starting)})
+                .attr("height", function(d) {return scale(d.height)})
                 .attr("opacity", function(d) {return d.opacity})
                 .attr("fill", function(d) {return app.colors[d.party]})
-                .attr("stroke", "none");
+                .attr("stroke", "none")
+                .call(rectTip)
+                .on("mouseover", rectTip.show)
+                .on("mouseleave", rectTip.hide);
 
+            var dots = groups.append("circle")
+                .attr("cx", barPadding + barWidth)
+                .attr("cy", function(d) {return height - scale(d.cost)})
+                .attr("r", 3)
+                .attr("fill", function(d) {
+                    if (d.cost > d.bars[0][0].ending) {
+                        return app.colors.red;
+                    }
+                    return app.colors.green;
+                });
 
-            // var bar = d3.select(selection).append("g") 
-            //     .datum(data);
-            //     // .attr("transform", function(d) {return "translate(" + (d.transform * (width + 2 * padding)) + ", 0)"});
+            var lines = groups.append("line")
+                .attr("x1", 0)
+                .attr("x2", 2 * barWidth)
+                .attr("y1", function(d) {return height - scale(d.cost)})
+                .attr("y2", function(d) {return height - scale(d.cost)})
+                .attr("fill", "none")
+                .attr("stroke", function(d) {
+                    if (d.cost > d.bars[0][0].ending) {
+                        return app.colors.red;
+                    }
+                    return app.colors.green;
+                });
 
-            // bar.selectAll("rect")
-            //     .data(data).enter().append("rect")
-            //     .attr("x", padding)
-            //     .attr("y", function(d) {return height - scale(d.ending)})
-            //     .attr("width", barWidth)
-            //     .attr("height", function(d) {return scale(d.ending) - scale(d.starting)})
-            //     .attr("opacity", function(d) {return d.opacity})
-            //     .attr("stroke", "none")
-            //     .attr("fill", "#666")
-            //     .on("mouseover", mouseover);
         }
 
         stackedBar.data = function(value) {
